4fdcd59fa423d2c2e9e0fc22aee8815fb870f9cb
Marek Dano <mk.dano@gmail.com>
2017-07-04 08:34:02 +0100

    Display default Google Maps in the Map component.

--- src/Map.jsx
+++ src/Map.jsx
@@ -2,13 +2,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+
+const InitialMap = withGoogleMap(props => {
+	console.log("In InitialMap")
+	return (
+		<GoogleMap
+			defaultZoom={8}
+			defaultCenter={{ lat: -34.397, lng: 150.644 }}
+		/>
+	)
+});
+
 
 class Map extends Component {
-
 	render() {
 		return (
-			<div>Map works!</div>
-		)
+			<div className="map">
+				<InitialMap
+					containerElement={
+						<div style={{ height: `100%` }} />
+					}
+					mapElement={
+						<div style={{ height: `100%` }} />
+					}
+				/>
+			</div>
+		
+		);
 	}
 }
 
