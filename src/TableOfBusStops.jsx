4c8e78200e7f8796bbd90692b71baaffe849f346
Marek Dano <mk.dano@gmail.com>
2017-07-12 20:13:41 +0100

    Display the table with bus stops data.

--- src/TableOfBusStops.jsx
+++ src/TableOfBusStops.jsx
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as _ from 'lodash';
 
 class TableOfBusStops extends Component {
@@ -8,16 +9,39 @@ class TableOfBusStops extends Component {
   }
 
 	render() {
-		console.log("Stops data", this.props.stopsData);
+		const stopsData = this.props.stopsData;
+		const tableBody = stopsData && stopsData.map(stop => {
+			return (
+				<tr>
+					<th>{stop.name}</th>
+					<th>{stop.students_count}</th>
+					<th>{stop.users_count}</th>
+					<th>{stop.routes_count}</th>
+				</tr>
+			)
+		})
 		return (
-			<div> TableOfBusStops works!</div>
+			<div>
+				<table>
+					<tr id="header">
+						<th>Stops table</th> 
+					</tr>
+					<tr>
+						<th>Stop Name</th>
+						<th>No Students</th>
+						<th>No Users</th>
+						<th>No Routes</th>
+					</tr>
+					{tableBody}
+				</table>
+
+			</div>
 		)
 	}
 }
 
-
+TableOfBusStops.propTypes = {
+  stopsData: PropTypes.array
+};
 
 export default TableOfBusStops;
-  
-
-  
